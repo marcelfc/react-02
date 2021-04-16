3c2685bbb4d083af6ba462aa59fa47074cc1e407
Marcel Cordeiro <marcelfcordeiro@gmail.com>
2021-04-16 17:48:09 -0300

    finishing

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
-  // Complete aqui
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Button } from "./Button";
+
+import { api } from '../services/api';
+import IGenreResponseProps from "../interfaces/IGenreResponseProps";
+
+interface SideBarProps {
+  setSelectedGenreId: Dispatch<SetStateAction<number>>;
+  selectedGenreId: number; 
+}
+
+export function SideBar(props: SideBarProps) {
+  const [genres, setGenres] = useState<IGenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<IGenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
+
 }
---
+++ src/interfaces/IMovieProps.ts
@@ -0,0 +1,10 @@
+export default interface IMovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
